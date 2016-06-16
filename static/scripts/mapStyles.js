c3682b3578e60a3fcb7e63d3d41ed04d48f27424
aryner <aryner@gmail.com>
2016-06-15 22:42:08 -0700

    added some preliminary styling to the map

---
+++ static/scripts/mapStyles.js
@@ -0,0 +1,73 @@
+var styles = [
+  {
+    featureType: 'poi',
+    elementType: 'all',
+    stylers: [
+      { saturation: -50 },
+      { lightness: -30 }
+    ]
+  },
+  {
+    featureType: 'water',
+    elementType: 'all',
+    stylers: [
+      { hue: '#008080' },
+      { saturation: 00 },
+      { lightness: -60 }
+    ]
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'all',
+    stylers: [
+      { hue: '#176117' },
+      { saturation: 0 },
+      { lightness: -30 }
+    ]
+  },
+  {
+    featureType: 'landscape.man_made',
+    elementType: 'all',
+    stylers: [
+      { hue: '#2f4f4f' },
+      { saturation: 0 },
+      { lightness: -50 }
+    ]
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'all',
+    stylers: [
+      { hue: '#ddd' },
+      { saturation: 0 },
+      { lightness: -50 }
+    ]
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'all',
+    stylers: [
+      { hue: '#b2b200' },
+      { saturation: 0 },
+      { lightness: -50 }
+    ]
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'all',
+    stylers: [
+      { hue: '#cccc00' },
+      { saturation: 0 },
+      { lightness: -40 }
+    ]
+  },
+  {
+    featureType: 'landscape.natural',
+    elementType: 'all',
+    stylers: [
+      { saturation: -50 },
+      { lightness: -10 }
+    ]
+  }
+];
+
--- static/scripts/maps.js
+++ static/scripts/maps.js
@@ -1,9 +1,14 @@
 var map;
+var styledMap = new google.maps.StyledMapType(styles,{name: "Styled Map"});
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.7598564, lng: -122.4349647},
     zoom: 12
   });
+
+  map.mapTypes.set('map_style',styledMap);
+  map.setMapTypeId('map_style');
 }
 
 initMap()
