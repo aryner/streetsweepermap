951aa6339f577bf982e984a9fb76cfd23c42ce7c
aryner <aryner@gmail.com>
2016-06-16 22:25:07 -0700

    zooming to level 16 or higher queries routes in the bounded region

--- static/scripts/maps.js
+++ static/scripts/maps.js
@@ -1,14 +1,40 @@
+// TODO
+// 2 - draw lines on the streets for the above query
+// 3 - split lines to either side of the street
+// 4 - create color gradient for distance from street cleaning 
+// 5 - apply above to the lines
+// 6 - prevent repeat queries and redrawing
+// 7 - style 
+
 var map;
 var styledMap = new google.maps.StyledMapType(styles,{name: "Styled Map"});
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.7598564, lng: -122.4349647},
-    zoom: 12
+    zoom: 15
   });
 
   map.mapTypes.set('map_style',styledMap);
   map.setMapTypeId('map_style');
+  map.addListener('bounds_changed', function() {
+    var zoom = map.getZoom();
+    if (zoom < 16) return;
+    console.log(zoom);
+
+    var bounds = map.getBounds();
+    var tl = {lat: bounds.H.j, lng: bounds.j.j};
+    var br = {lat: bounds.H.H, lng: bounds.j.H};
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+        console.log(xmlHttp.responseText);
+      }
+    }
+    xmlHttp.open("GET", "http://localhost:5000/routes/"+br.lat+"&"+tl.lng+"&"+tl.lat+"&"+br.lng, true);
+    xmlHttp.send(null);
+  });
   
   return map;
 }
@@ -26,8 +52,7 @@ function drawLine(map, coords, color) {
 
 map = initMap()
 
-// 37.7598248, -122.4573765
-// 37.7405511, -122.4156628
+// random line
 var coords = [{lat: 37.7598248, lng: -122.4573765},{lat: 37.7405511, lng: -122.4156628}]
 drawLine(map, coords, '#ff0000')
 
