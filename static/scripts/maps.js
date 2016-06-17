b703148847737d3cc59c66794a70ce95dd2fafc4
aryner <aryner@gmail.com>
2016-06-16 20:17:51 -0700

    added a draw line function to draw lines on the map

--- static/scripts/maps.js
+++ static/scripts/maps.js
@@ -9,6 +9,26 @@ function initMap() {
 
   map.mapTypes.set('map_style',styledMap);
   map.setMapTypeId('map_style');
+  
+  return map;
 }
 
-initMap()
+function drawLine(map, coords, color) {
+  var line = new google.maps.Polyline({
+    path: coords,
+    strokeColor: color,
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+  });
+
+  line.setMap(map);
+}
+
+map = initMap()
+
+// 37.7598248, -122.4573765
+// 37.7405511, -122.4156628
+var coords = [{lat: 37.7598248, lng: -122.4573765},{lat: 37.7405511, lng: -122.4156628}]
+drawLine(map, coords, '#ff0000')
+
+
